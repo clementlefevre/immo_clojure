374f199f5740418caeadbc824f876655fe888557
clementlefevre <clement.san@gmail.com>
2021-12-12 14:51:06 +0100

    update

--- src/immo_clojure/curl_to_clj_http.clj
+++ src/immo_clojure/curl_to_clj_http.clj
@@ -2,7 +2,8 @@
                                    [clojure.core]
                                    [clojure.pprint]
                                    [clj-http.client]
-                                   [cheshire.core :as cc]))
+                                   [cheshire.core :as cc]
+                                   [net.cgrand.enlive-html :as html]))
 
 (defn get-www-locals-de [page-index] (let [t (clj-http.client/post
                                               "https://www.locals.de/"
@@ -30,17 +31,19 @@
 (type output)
 (clojure.pprint/pprint output)
 
-(slurp "https%3A%2F%2Fsmartsite2.myonoffice.de%2Fkunden%2Fagasimmobilien%2F17%2Fimmobilien-nach-vermarktungsstatus.xhtml%3Fs%5C%5B20999-39%5C%5D%3D1%26s%5C%5B20999-37%5C%5D%3D1%26p%5C%5Bobj0%5C%5D%3D5")
 
 
 (defn get-agas-immobilien [] (let [t (clj-http.client/get
                                       "https://smartsite2.myonoffice.de/kunden/agasimmobilien/17/immobilien-nach-vermarktungsstatus.xhtml?s[20999-39]=1&s[20999-37]=1&p[obj0]=5"
                                       {:headers
                                        {:user-agent "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36"
-                                        :accept "application/json, text/javascript, */*; q=0.01'"}})]
-                               (:body t)))
+                                        :accept "application/json, text/javascript, */*; q=0.01'"
+                                        :content-type "application/x-www-form-urlencoded; charset=UTF-8"}})]
+                               t))
 
 (def result (get-agas-immobilien))
-(def parsed (cc/parse-string result))
-(println result)
+(type (:body result))
+(def parsed-html (html/html-snippet (:body result)))
+(clojure.pprint/pprint parsed-html)
+(html/select parsed-html [:div])
 ;Niedrigenergiehaus
